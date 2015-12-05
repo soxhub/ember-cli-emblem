4ac681f5e4bc3f1c9e52f17aa2b02104373c16b3
joseph bartels <joebartels@hotmail.com>
2015-12-05 18:00:49 -0500

    lots update lots o' dependencies, add ember try

--- tests/acceptance/application-renders-test.js
+++ tests/acceptance/application-renders-test.js
@@ -15,9 +15,13 @@ module('Acceptance: ApplicationRendersTest', {
 });
 
 test('visiting /', function(assert) {
+  assert.expect(3);
+
   visit('/');
 
   andThen(function() {
     assert.equal(Ember.$('h2#title').html(),"Welcome to Ember.js");
+    assert.equal(Ember.$('p.para').html(), "This is a paragraph that compiles to Handlebars.js");
+    assert.equal(Ember.$('h4.header').html(), "This is an h4");
   });
 });
